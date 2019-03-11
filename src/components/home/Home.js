9fe7576b60d47e117fe2ff5a3097a2bfbadc6e9e
Claire Flanagan <claireflanagan@gmail.com>
2019-03-10 20:43:00 -0700

    more spacing fixes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import {
-  Link
-} from 'react-router-dom';
-import {
-  ROUTES
-} from '../../routes/routes';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../routes/routes';
 import styles from './Home.css';
 
 export default function Home() {
