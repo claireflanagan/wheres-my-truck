e8912b2a8f9f5faa588ebb6b85968f89d28bac90
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-07-16 11:24:38 -0700

    Add handleChange and onChange method back into form

--- src/components/admin/TruckForm.js
+++ src/components/admin/TruckForm.js
@@ -28,6 +28,10 @@ export default function TruckForm({ match }) {
     };
   }
 
+  const handleChange = value => {
+    console.log('value', value);
+  };
+
   const handleFileChange = ({ target }) => {
     this.setState({
       [target.name]: target.value,
@@ -48,47 +52,47 @@ export default function TruckForm({ match }) {
       <form onSubmit={handleSubmit} className={styles.form}>
         <p>
           <label>Truck Name:</label>
-          <input required={true} name="name" type="text" defaultValue={truck.name} />
+          <input required={true} name="name" type="text" value={truck.name} onChange={handleChange} />
         </p>
         <p>
           <label>Current Location:</label>
-          <input required={true} name="location" type="text" defaultValue={truck.location} />
+          <input required={true} name="location" type="text" value={truck.location} onChange={handleChange} />
         </p>
         <p>
           <label>Vin:</label>
-          <input required={true} name="vin" type="text" defaultValue={truck.vin} />
+          <input required={true} name="vin" type="text" value={truck.vin} onChange={handleChange} />
         </p>
         <p>
           <label>Plates:</label>
-          <input required={true} name="plates" type="text" defaultValue={truck.plates} />
+          <input required={true} name="plates" type="text" value={truck.plates} onChange={handleChange} />
         </p>
         <p>
           <label>Year:</label>
-          <input required={true} name="year" type="year" defaultValue={truck.year} />
+          <input required={true} name="year" type="year" value={truck.year} onChange={handleChange} />
         </p>
         <p>
           <label>Make:</label>
-          <input required={true} name="make" type="text" defaultValue={truck.make} />
+          <input required={true} name="make" type="text" value={truck.make} onChange={handleChange} />
         </p>
         <p>
           <label>Model:</label>
-          <input required={true} name="model" type="text" defaultValue={truck.model} />
+          <input required={true} name="model" type="text" value={truck.model} onChange={handleChange} />
         </p>
         <p>
           <label>Tire Size:</label>
-          <input required={true} name="tireSize" type="number" defaultValue={truck.tireSize} />
+          <input required={true} name="tireSize" type="number" value={truck.tireSize} onChange={handleChange} />
         </p>
         <p>
           <label>Date of Purchase:</label>
-          <input required={true} name="boughtDate" type="date" defaultValue={truck.boughtDate} />
+          <input required={true} name="boughtDate" type="date" value={truck.boughtDate} onChange={handleChange} />
         </p>
         <p>
           <label>Proof of Registration:</label>
-          <input name="registration" type="file" accept=".jpg, .png, .svg, .gif" defaultValue={truck.registration} onChange={handleFileChange} />
+          <input name="registration" type="file" accept=".jpg, .png, .svg, .gif" value={truck.registration} onChange={handleFileChange} />
         </p>
         <p>
           <label>Proof of Insurance:</label>
-          <input name="insurance" type="file" accept=".jpg, .png, .svg, .gif" defaultValue={truck.insurance} onChange={handleFileChange} />
+          <input name="insurance" type="file" accept=".jpg, .png, .svg, .gif" value={truck.insurance} onChange={handleFileChange} />
         </p>
         <button type="submit">Save</button>
       </form>
