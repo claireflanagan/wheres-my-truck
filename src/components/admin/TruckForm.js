990de05f46af3a7962682bb52d8ed06f8d6d7287
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-07-16 09:59:53 -0700

    Remove onChange prop and methods and change value to defaultValue to use hooks properly

--- src/components/admin/TruckForm.js
+++ src/components/admin/TruckForm.js
@@ -8,7 +8,6 @@ import styles from './AddTruck.css';
 
 export default function TruckForm({ match }) {
   let truck = useFirebase(trucksCollection.doc(match.params.id));
-  console.log('truck', truck);
   if(truck === undefined) return <Loading />;
 
   if(truck === null) {
@@ -36,10 +35,6 @@ export default function TruckForm({ match }) {
     });
   };
 
-  const handleChange = ({ target }) => {
-    this.setState({ [target.name]: target.value });
-  };
-
   const handleSubmit = event => {
     event.preventDefault();
     const truck = this.state;
@@ -53,47 +48,47 @@ export default function TruckForm({ match }) {
       <form onSubmit={handleSubmit} className={styles.form}>
         <p>
           <label>Truck Name:</label>
-          <input required={true} name="name" type="text" value={truck.name} onChange={handleChange} />
+          <input required={true} name="name" type="text" defaultValue={truck.name} />
         </p>
         <p>
           <label>Current Location:</label>
-          <input required={true} name="location" type="text" value={truck.location} onChange={handleChange} />
+          <input required={true} name="location" type="text" defaultValue={truck.location} />
         </p>
         <p>
           <label>Vin:</label>
-          <input required={true} name="vin" type="text" value={truck.vin} onChange={handleChange} />
+          <input required={true} name="vin" type="text" defaultValue={truck.vin} />
         </p>
         <p>
           <label>Plates:</label>
-          <input required={true} name="plates" type="text" value={truck.plates} onChange={handleChange} />
+          <input required={true} name="plates" type="text" defaultValue={truck.plates} />
         </p>
         <p>
           <label>Year:</label>
-          <input required={true} name="year" type="year" value={truck.year} onChange={handleChange} />
+          <input required={true} name="year" type="year" defaultValue={truck.year} />
         </p>
         <p>
           <label>Make:</label>
-          <input required={true} name="make" type="text" value={truck.make} onChange={handleChange} />
+          <input required={true} name="make" type="text" defaultValue={truck.make} />
         </p>
         <p>
           <label>Model:</label>
-          <input required={true} name="model" type="text" value={truck.model} onChange={handleChange} />
+          <input required={true} name="model" type="text" defaultValue={truck.model} />
         </p>
         <p>
           <label>Tire Size:</label>
-          <input required={true} name="tireSize" type="number" value={truck.tireSize} onChange={handleChange} />
+          <input required={true} name="tireSize" type="number" defaultValue={truck.tireSize} />
         </p>
         <p>
           <label>Date of Purchase:</label>
-          <input required={true} name="boughtDate" type="date" value={truck.boughtDate} onChange={handleChange} />
+          <input required={true} name="boughtDate" type="date" defaultValue={truck.boughtDate} />
         </p>
         <p>
           <label>Proof of Registration:</label>
-          <input name="registration" type="file" accept=".jpg, .png, .svg, .gif" value={truck.registration} onChange={handleFileChange} />
+          <input name="registration" type="file" accept=".jpg, .png, .svg, .gif" defaultValue={truck.registration} onChange={handleFileChange} />
         </p>
         <p>
           <label>Proof of Insurance:</label>
-          <input name="insurance" type="file" accept=".jpg, .png, .svg, .gif" value={truck.insurance} onChange={handleFileChange} />
+          <input name="insurance" type="file" accept=".jpg, .png, .svg, .gif" defaultValue={truck.insurance} onChange={handleFileChange} />
         </p>
         <button type="submit">Save</button>
       </form>
