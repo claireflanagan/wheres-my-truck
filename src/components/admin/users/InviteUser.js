1c3425817712de56c208df4b8a7aa9793dd49f7b
Claire Flanagan <claireflanagan@gmail.com>
2019-07-28 15:59:13 -0700

    labels added and css connected

--- src/components/admin/users/InviteUser.js
+++ src/components/admin/users/InviteUser.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { inviteUser } from '../../../actions/admin';
+import styles from '../../trip/TruckCheckoutForm.css';
 
 export default function InviteUser() {
   const [email, setEmail] = useState('');
@@ -14,8 +15,10 @@ export default function InviteUser() {
   const handleRoleChange = ({ target }) => setRole(target.value);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={[styles.form, styles.userForm].join(' ')}>
+      <label>Email:</label>
       <input type="email" value={email} onChange={handleEmailChange} />
+      <label>Role:</label>
       <select defaultValue={role} onChange={handleRoleChange}>
         <option value="user">user</option>
         <option value="admin">admin</option>
